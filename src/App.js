c46d12b8803907de4b7cacd6e7c94cf1cc94a065
ThibaultHareau <thibault.hareau6@etu.univ-lorraine.fr>
2021-05-11 19:32:47 +0200

    Ajout image - test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="Recap-zone">
+        <img src="..\img\strong.jpg" className="Recap-img" alt="img" />
+        <p className="recap">
+          Le travail 24/7 ça marche !
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
