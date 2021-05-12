9ac2229698f53f84dcc3f13d25b0107cc84ac2d3
ThibaultHareau <thibault.hareau6@etu.univ-lorraine.fr>
2021-05-12 19:30:28 +0200

    CSS commenté

--- src/App.js
+++ src/App.js
@@ -6,20 +6,21 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="content">
+        <div className="main_content">
           <h1>Bienvenue Visiteur !</h1>
           <h4> Voici ton évolution</h4>
           <img src="img\strong.png" alt="muscle" className="evolution"/>
-          <div id="comment">BRAVO !</div>
+          <div id="general_comment">
+            <div id="title_general_comment">BRAVO !</div>
+            <div id="text_general_comment">Vous avez bien travaillé !</div>
+          </div>
         </div>
         <div className="menu">
-          <ul>
-            <li><input type="button" value="Accueil"/></li>
-            <li><input type="button" value="Ma dernière séance"/></li>
-            <li><input type="button" value="Mes accomplissements"/></li>
-            <li><input type="button" value="Mes progrès"/></li>
-            <li><input type="button" value="Profil"/></li>
-          </ul>
+          <input type="button" value="Accueil" id="home_button"/>
+          <input type="button" value="Ma dernière séance" id="last_session_button"/>
+          <input type="button" value="Mes accomplissements" id="accomplishment_button"/>
+          <input type="button" value="Mes progrès" id="progress_button"/>
+          <input type="button" value="Profil" id="profile_button"/>
         </div>
       </div>
     );
