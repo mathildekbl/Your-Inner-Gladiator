d4b29fb5a9f9d01e019e679c2b87f2cd0b4df890
ThibaultHareau <thibault.hareau6@etu.univ-lorraine.fr>
2021-05-12 17:52:33 +0200

    Quelques tests CSS + ajout bar latérale

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-
-        <h1>Bienvenue Visiteur !</h1>
-
-        <h4> Voici ton évolution</h4>
-
-        <img src="img\strong.png" alt="muscle" className="evolution"/>
-
-        <div id="comment">BRAVO !</div>
-
+        <div className="content">
+          <h1>Bienvenue Visiteur !</h1>
+          <h4> Voici ton évolution</h4>
+          <img src="img\strong.png" alt="muscle" className="evolution"/>
+          <div id="comment">BRAVO !</div>
+        </div>
+        <div className="menu">
+          <ul>
+            <li><input type="button" value="Accueil"/></li>
+            <li><input type="button" value="Ma dernière séance"/></li>
+            <li><input type="button" value="Mes accomplissements"/></li>
+            <li><input type="button" value="Mes progrès"/></li>
+            <li><input type="button" value="Profil"/></li>
+          </ul>
+        </div>
       </div>
     );
   }
